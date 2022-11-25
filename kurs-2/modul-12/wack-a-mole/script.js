43490b1ca2b885d6bbb92ed9d49cec31ee0c2639
Fredrik <io.qrdr@gmail.com>
2022-11-25 03:06:06 +0100

    add restart button

--- kurs-2/modul-12/wack-a-mole/script.js
+++ kurs-2/modul-12/wack-a-mole/script.js
@@ -51,22 +51,40 @@ class Game {
         this.#state.timer = setInterval(() => this.#timer(), 1000);
     }
 
+    restart() {
+        document.querySelector('footer').classList.toggle('hidden');
+        
+        this.start();
+
+        document.querySelector('.moleswhacked b').innerHTML = this.#state.score;
+        document.querySelector('.timeleft b').innerHTML = `${this.#state.time}s`;
+    }
+
     #loop() {
         this.#mole.peek();
         setTimeout(() => this.#mole.hide(), this.#appearanceSpeed);
+    }
+
+    #endGame() {
+        this.#mole.hide();
+
+        clearInterval(this.#state.timer);
+        clearInterval(this.#state.loop);
+
+        document.querySelector('footer').classList.toggle('hidden');
+        document.querySelector('.scored').innerHTML =
+            `You whacked <b>${this.#state.score}</b> moles in 60 sec.`;
     }
 
     #timer() {
         if (this.#state.time >= 0) {
             document.querySelector('.timeleft b').innerHTML = `${this.#state.time--}s`;
         } else {
-            this.#mole.hide();
-            clearInterval(this.#state.timer);
-            clearInterval(this.#state.loop);
-            alert(`You whacked ${this.#state.score} moles in 60 sec.`);
+            this.#endGame();
         }
     }
 }
 
 const game = new Game();
 game.start();
+document.querySelector('footer button').addEventListener('click', () => game.restart());
