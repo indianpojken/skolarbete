935da7d73cc27ce699be9bdcaa6675fe62290b43
indianpojken <io.qrdr@gmail.com>
2023-03-21 22:27:02 +0100

    seperate validator for cart

--- kurs-6/modul-3/sinus-webshop-api/routes/cart.js
+++ kurs-6/modul-3/sinus-webshop-api/routes/cart.js
@@ -11,40 +11,32 @@ router.get('/', (request, response) => {
   response.status(200).json({ items: cart.items, total: cart.total });
 });
 
-router.post('/',
-  validate({
-    body: {
-      serial: { type: 'string' },
-    },
-  }),
-  (request, response) => {
-    const { serial } = request.body;
+const validator = validate({
+  body: {
+    serial: { type: 'string' },
+  },
+});
 
-    try {
-      cart.add(serial);
-      response.status(201).json({ success: true });
-    } catch (error) {
-      response.status(400).json({ success: false, message: error.message });
-    }
+router.post('/', validator, (request, response) => {
+  const { serial } = request.body;
+
+  try {
+    cart.add(serial);
+    response.status(201).json({ success: true });
+  } catch (error) {
+    response.status(400).json({ success: false, message: error.message });
   }
-);
+});
 
-router.delete('/',
-  validate({
-    body: {
-      serial: { type: 'string' },
-    },
-  }),
-  (request, response) => {
-    const { serial } = request.body;
+router.delete('/', validator, (request, response) => {
+  const { serial } = request.body;
 
-    try {
-      cart.remove(serial);
-      response.status(201).json({ success: true });
-    } catch (error) {
-      response.status(400).json({ success: false, message: error.message });
-    }
+  try {
+    cart.remove(serial);
+    response.status(200).json({ success: true });
+  } catch (error) {
+    response.status(400).json({ success: false, message: error.message });
   }
-);
+});
 
 export default router;
